60f687a9293fb84f231fa36db821c36a3d18854e
Alamin <mdalaminhossain573190@gmail.com>
2024-04-22 11:19:47 +0600

    first commit

---
+++ src/componants/Users.jsx
@@ -0,0 +1,32 @@
+import { useLoaderData } from "react-router-dom";
+
+
+const Users = () => {
+    const users = useLoaderData()
+    const handleDeleteUser = _id =>{
+        console.log('delete',_id)
+        fetch(`http://localhost:5000/users/${_id}`,{
+            method:'DELETE'
+        })
+        .then(res=>res.json())
+        .then(data =>{
+            console.log(data)
+            if(data.deletedCound>0){
+                alert('deleted successfully');
+            }
+        })
+
+    }
+    return (
+        <div>
+            <h2>{users.length}</h2>
+            <div>
+                {
+                    users.map(user =><p key={user._id}>{user.name} : {user.email}:{user._id} <button onClick={() => handleDeleteUser(user._id)}>X</button> </p> )
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Users;
